a090f72e4509ece4e0cc493e38aaff77403944e2
oBrenoB <116448700+oBrenoB@users.noreply.github.com>
2024-09-19 15:37:46 -0300

    Update page.tsx

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -16,6 +16,7 @@ export default async function IndexPage() {
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
+      <h1>Hello World</h1>
       <Chat id={id} session={session} missingKeys={missingKeys} />
     </AI>
   )
